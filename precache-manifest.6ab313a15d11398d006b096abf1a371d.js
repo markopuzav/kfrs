dd543f9a2d5bb5e525bdabc0dbbae49268014bf0
markopuzav <marko.puza@vacuumlabs.com>
2020-04-28 20:36:43 +0200

    Updates

---
+++ precache-manifest.6ab313a15d11398d006b096abf1a371d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1986931f433c39f497bc429d6c64d3e",
+    "url": "/kfrs/index.html"
+  },
+  {
+    "revision": "666813e95544d20305a3",
+    "url": "/kfrs/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "07360e946e0f89c49f33",
+    "url": "/kfrs/static/js/2.5dfd1b39.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/kfrs/static/js/2.5dfd1b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666813e95544d20305a3",
+    "url": "/kfrs/static/js/main.aa4f4d62.chunk.js"
+  },
+  {
+    "revision": "55e59d35a207d4a10dda",
+    "url": "/kfrs/static/js/runtime-main.2972bff3.js"
+  }
+]);
